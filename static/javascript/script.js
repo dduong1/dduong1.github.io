2b5fef87e87fc68c455b679f97429e0017c28fe7
dduong1 <duong.david01@gmail.com>
2021-01-28 16:17:57 +0100

    Add files via upload

---
+++ static/javascript/script.js
@@ -0,0 +1,105 @@
+
+
+
+function test(){
+    console.log("pepe assessment");
+}
+
+
+function postUpdateBox(elem,item, aID){
+    $('#' + elem ).empty();
+    if (item == 1){
+    $('#' + elem ).html('<p class="text-danger">Not in place</p>');
+    }else if(item == 2){
+    $('#' + elem ).html('<p class="text-warning">In place but not compliant</p>');
+    }else if(item == 3){
+    $('#' + elem ).html('<p class="text-secondary">Partially compliant with significant adjustments required</p>');
+    }else if(item == 4){
+    $('#' + elem ).html('<p class="text-info">Partially compliant with minor adjustments required</p>');
+    }else if(item == 5){
+    $('#' + elem ).html('<p class="text-success">Fully compliant</p>');
+    }else if(item == 0){
+    $('#' + elem ).html('<p class="text-success">Not applicable</p>');
+    }
+
+    $.ajax({
+        type: "POST",
+        url: "/assessment/postUpdateBox",
+        data: {"aID" : aID, "val": item },
+        success: function(data) {
+            console.log("OK")
+        }
+    });
+}
+
+
+function postComment(aID){
+    txtarea = $('#' + aID + '_comment').val();
+    $.ajax({
+        type: "POST",
+        url: "/assessment/postComment",
+        data: {"aID" : aID, "text": txtarea},
+        success: function(data) {
+            console.log("OK")
+        }
+    });
+}
+
+//function activateScroll(){
+//    window.onscroll = function() {scrollFunction()};
+//    console.log("activateScroll");
+//}
+
+function getLab(arr){
+    //console.log($("#qContainer").scrollTop() + "        " + (arr.offsetTop).toString());
+    return (arr.offsetTop) <= $("#qContainer").scrollTop();
+}
+
+
+function getsectionpos(sections){
+
+//    sections.each( function( index, element ){
+//        console.log( $( this ).position()['top'] );
+//    });
+    tmp = [...sections].reverse().findIndex(getLab)
+    if (tmp >=0){
+        zz = sections.length - 1 - tmp ;
+        
+        //$('#section-top').html('<b>' + sections[zz].textContent + '</b>' ); //sections[zz].firstElementChild.textContent + ' ' +
+    }else
+    {
+        zz = 0 ;
+    }
+    //console.log(sections[zz].firstElementChild.textContent);
+    
+}
+
+
+function scrollFunction() {
+    const sections = $(".template__section");
+    // console.log($("#qContainer").scrollTop() );
+    getsectionpos(sections);
+
+
+    if ($("#qContainer").scrollTop() >= 0  && $("#qContainer").scrollTop() <130 ) { //
+
+        $("#qHeader").height(170- $("#qContainer").scrollTop());        
+        $("#qHeader").css("opacity", "1");
+        $("#qHeader").css("blur", "0px");
+        
+        $("#stats-container").height(170- $("#qContainer").scrollTop());}
+
+    else if ($("#qContainer").scrollTop() >= 130 && $("#qContainer").scrollTop() < 170){
+        var c = 1-($("#qContainer").scrollTop()-130)/39;
+        $("#stats-container").css("color","rgba(239, 239, 239,"+ c +")") // " + 1-($("#qContainer").scrollTop()-130)/39 + ")");
+        console.log(1-($("#qContainer").scrollTop()-130)/39 );
+    }
+    
+    else {
+        
+        $("#qHeader").height(40);
+        $("#stats-container").height(0);
+        $("#qHeader").css("opacity", "0.9");
+        $("#qHeader").css("blur", "20px");
+    }
+}
